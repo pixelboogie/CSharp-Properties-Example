eec7222596a7c7a9d6c0819d798a1ee7b53c393f
pixelboogie <41452340+pixelboogie@users.noreply.github.com>
2021-03-12 13:16:16 -0800

    Add files via upload

---
+++ Game.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour 
+{
+    void Start () 
+    {
+        Debug.Log("Game Started ");
+        
+        Player myPlayer = new Player();
+
+        //Properties can be used just like variables
+        myPlayer.Experience = 5000f;
+        float exp = myPlayer.Experience;
+        float lvl = myPlayer.Level;
+        float hlth = myPlayer.Health;
+        
+
+        Debug.Log("Game experience = " + exp);
+        Debug.Log("Game level = " + lvl);
+        Debug.Log("Game health = " + hlth);
+
+         Debug.Log("Game Start ended ");
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player
+{
+    //Member variables can be referred to as
+    //fields.
+    private float experience;
+
+    //Experience is a basic property
+    public float Experience
+    {
+        get
+        {
+           
+            Debug.Log("inside Experience get");
+            return experience;
+        }
+        set
+        {
+            Debug.Log("inside Experience set");
+            experience = value;
+        }
+    }
+
+    //Level is a property that converts experience
+    //points into the leve of a player automatically
+    public float Level
+    {
+        get
+        {
+             Debug.Log("Level get");
+            return experience / 100f;
+        }
+        set
+        {
+            Debug.Log("Level set");
+            experience = value * 100f;
+        }
+    }
+
+    //This is an example of an auto-implemented
+    //property
+    public float Health{ get; set;}
+
+
+    public Player (){
+        Debug.Log("Player constructor method starts.");
+        Debug.Log("Experience is " + Experience);
+        Debug.Log("Level is " + Level);
+        Debug.Log("Health is " + Health);
+        Debug.Log("Player constructor method ends.");
+    }
+}
